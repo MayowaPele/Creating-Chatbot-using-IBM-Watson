37c9af538a74d13583c09a677fe46a128eab8e0e
Grateful Mayowa Pelemo <mayowapele10@gmail.com>
2025-11-11 20:14:57 +0000

    Add files via upload

--- Chatbot code.py
+++ Chatbot code.py
@@ -3,6 +3,7 @@ from ibm_watsonx_ai.foundation_models import ModelInference
 from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
 from ibm_watsonx_ai import Credentials
 from langchain_ibm import WatsonxLLM
+import gradio as gr
 
 # Specify the model and project settings 
 # (make sure the model you wish to use is commented out, and other models are commented)
@@ -25,8 +26,19 @@ watsonx_llm = WatsonxLLM(
     params=parameters,
 )
 
-# Get the query from the user input
-query = input("Please enter your query: ")
+# Function to generate a response from the model
+def generate_response(prompt_txt):
+    generated_response = watsonx_llm.invoke(prompt_txt)
+    return generated_response
 
-# Print the generated response
-print(watsonx_llm.invoke(query))
+# Create Gradio interface
+chat_application = gr.Interface(
+    fn=generate_response,
+    allow_flagging="never",
+    inputs=gr.Textbox(label="Input", lines=2, placeholder="Type your question here..."),
+    outputs=gr.Textbox(label="Output"),
+    title="Watsonx.ai Chatbot",
+    description="Ask any question and the chatbot will try to answer."
+)
+# Launch the app
+chat_application.launch(server_name="127.0.0.1", server_port= 7860)
